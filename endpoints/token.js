24f6821d7aba2117572e08e19dd547118f73a565
Dan Jesus <danper.jesus@gmail.com>
2016-07-26 01:34:19 -0300

    Add apidoc to token endpoint

--- endpoints/token.js
+++ endpoints/token.js
@@ -5,6 +5,28 @@ import jwt from 'jsonwebtoken';
 module.exports = app => {
   const User = app.db.models.User;
 
+  /**
+   * @api {post} /token Authenticated Token
+   * @apiGroup Crendential
+   * @apiParam {String} email User email
+   * @apiParam {String} password User password
+   * @apiParamExample {json} entry
+   *    {
+   *      "email": "steve.woz@apple.com",
+   *      "password": "123456"
+   *    }
+   * @apiSuccess {String} token Authenticated user token
+   * @apiSuccessExample {json} Success
+   *    HTTP/1.1 200 OK
+   *    {
+   *      "token": "token.xpto"
+   *    }
+   * @apiErrorExample {json} Authentication error
+   *    HTTP/1.1 401 Unauthorized
+   * 
+   * @apiErrorExample {json} Invalid parameters
+   *    HTTP/1.1 400 Bad Request
+   */
   app.post('/token', (req, res) => {
     if (!req.body.email || !req.body.password) {
       return res.sendStatus(400);
