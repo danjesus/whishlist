976cc6036826e150bd0e29740ca0e0a4e2398eda
Dan Jesus <danper.jesus@gmail.com>
2016-07-26 01:25:32 -0300

    Implement apidoc

---
+++ endpoints/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = app => {
+   /**
+   * @api {get} / API Status
+   * @apiGroup Status
+   * @apiSuccess {String} status Api status message
+   * @apiSuccessExample {json} Success
+   *    HTTP/1.1 200 OK
+   *    {
+   *      "status": "Whishlist API ready"
+   *    }
+   */
+  app.get('/', (req, res) => {
+    res
+      .status(200)
+      .json({
+        status: 'Whishlist API ready'
+      });
+  });
+}
