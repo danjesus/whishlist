275f9ef1ac4ab9aae3f9e37c11e7c0f4d92e07bb
Dan Jesus <danper.jesus@gmail.com>
2016-07-26 01:07:18 -0300

    Add user endpoint list and update

--- endpoints/users.js
+++ endpoints/users.js
@@ -1,15 +1,37 @@
 'use strict';
 
+import PasswordFactory from '../lib/password.factory';
+
 module.exports = app => {
-    app.get('/api/user/:id', (req, res) => {
 
-    });
+  const User = app.db.models.User;
 
-    app.put('/api/user/:id', (req, res) => {
-
-    });
-
-    app.delete('api/user', (req, res) => {
-
+  app.route('/user')
+    .all(app.auth.authenticate())
+    .get((req, res) => {
+      User.findById(req.user.id, {
+        attributes: ['name', 'email', 'full_address', 'created_at', 'updated_at']
+      })
+        .then(user => {
+          res.status(200).json(user);
+        })
+        .catch(err => {
+          res.status(412).json({ message: err.message });
+        });
+    })
+    .put((req, res) => {
+      if (req.body.password) {
+        req.body.password = PasswordFactory.create(req.body.password);
+      }
+      
+      User.update(req.body, {
+        where: {
+          id: req.user.id
+        }
+      })
+        .then(result => res.sendStatus(204))
+        .catch(err => {
+          res.status(412).json({ message: err.message });
+        });
     });
 };
---
+++ test/functional/endpoints/user.spec.js
@@ -0,0 +1,40 @@
+'use strict';
+
+import {createUser, emptyUsers} from './fixture';
+import jwt from 'jsonwebtoken';
+
+describe('User route', () => {
+
+  let token;
+
+  beforeEach(done => {
+    createUser().then(user => {
+      token = jwt.sign({ id: user.id }, app.get('jwtSecret'));
+      done();
+    });
+  });
+
+  afterEach(done => {
+    emptyUsers().then(() => done());
+  });
+
+  it('should return user logged', done => {
+    request.get('/user')
+      .set('Authorization', `JWT ${token}`)
+      .expect(200)
+      .end((err, res) => done(err));
+  });
+
+  it('should update user data', done => {
+    request.put('/user')
+      .send({
+        name: 'Steve woz',
+        email: 'steve@apple.com',
+        password: 'newpass',
+        zipcode: '04195020'
+      })
+      .set('Authorization', `JWT ${token}`)
+      .expect(204)
+      .end((err, res) => done(err));
+  });
+})
